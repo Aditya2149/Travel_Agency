15afef3a2cb0ed10398f256d63565b487fec5553
Aditya Kumar <94384575+Aditya2149@users.noreply.github.com>
2024-12-17 02:19:38 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,6 @@ const app = express();
 // Enable CORS for all origins
 app.use(cors({ origin: '*' })); // Allow all origins
 
-app.get('/api/packages', (req, res) => {
-    res.json({ message: 'Packages fetched successfully' });
-});
-
 // Middleware for parsing JSON requests
 app.use(express.json());
 
